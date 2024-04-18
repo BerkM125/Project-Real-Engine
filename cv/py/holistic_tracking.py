bf135833672ffed4c8bb0e7ec0c9b1dd9327df99
BerkM125 <49427416+BerkM125@users.noreply.github.com>
2024-04-18 00:37:34 -0700

    added 3D spatial data logging, organized code

--- cv/py/holistic_tracking.py
+++ cv/py/holistic_tracking.py
@@ -1,82 +1,55 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_holistic = mp.solutions.holistic
 
-# For static images:
 IMAGE_FILES = []
-BG_COLOR = (192, 192, 192) # gray
-with mp_holistic.Holistic(
-    static_image_mode=True,
-    model_complexity=0,
+RESULT_BUFFER = []
+BG_COLOR = (70, 70, 70)
+
+def process_results():
+  global RESULT_BUFFER
+  dump_file = open("./out/coordinate_dump.obj", "w")
+
+  for res in RESULT_BUFFER:
+    dump_file.write("# Pose coords: \n")
+    if type(res.pose_landmarks) is not type(None):
+        for idx in range(len(res.pose_landmarks.landmark)):
+            curr_pose_lm = res.pose_landmarks.landmark[idx]
+            pl = curr_pose_lm
+            dump_file.write(str("v " + str(pl.x) + " " + str(pl.y) + " " + str(pl.z) + " 1.0\n"))
+
+    if type(res.left_hand_landmarks) is not type(None):
+        dump_file.write("# Left Hand coords: \n")
+        for idx in range(len(res.left_hand_landmarks.landmark)):
+            curr_hand_lm = res.left_hand_landmarks.landmark[idx]
+            hl = curr_hand_lm
+            dump_file.write(str("v " + str(hl.x) + " " + str(hl.y) + " " + str(hl.z) + " 1.0\n"))
+
+    dump_file.write("# Right Hand coords: \n")
+
+    if type(res.right_hand_landmarks) is not type(None):
+        for idx in range(len(res.right_hand_landmarks.landmark)):
+            curr_hand_lm = res.right_hand_landmarks.landmark[idx]
+            hl = curr_hand_lm
+            dump_file.write(str("v " + str(hl.x) + " " + str(hl.y) + " " + str(hl.z) + " 1.0\n"))
+
+  dump_file.close()
+
+cap = cv2.VideoCapture(0)
+with mp_holistic.Holistic(    
+    model_complexity=1,
+    static_image_mode=False,
     enable_segmentation=True,
     refine_face_landmarks=False,
-    min_detection_confidence=0.3,
-    min_tracking_confidence=0.3,
-    smooth_segmentation=True) as holistic:
-  
-  for idx, file in enumerate(IMAGE_FILES):
-    image = cv2.imread(file)
-    image_height, image_width, _ = image.shape
-    # Convert the BGR image to RGB before processing.
-    results = holistic.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-    if results.pose_landmarks:
-      print(
-          f'Nose coordinates: ('
-          f'{results.pose_landmarks.landmark[mp_holistic.PoseLandmark.NOSE].x * image_width}, '
-          f'{results.pose_landmarks.landmark[mp_holistic.PoseLandmark.NOSE].y * image_height})'
-      )
-
-    annotated_image = image.copy()
-    # Draw segmentation on the image.
-    # To improve segmentation around boundaries, consider applying a joint
-    # bilateral filter to "results.segmentation_mask" with "image".
-    condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
-    bg_image = np.zeros(image.shape, dtype=np.uint8)
-    bg_image[:] = BG_COLOR
-    annotated_image = np.where(condition, annotated_image, bg_image)
-    # Draw pose, left and right hands, and face landmarks on the image.
-    # mp_drawing.draw_landmarks(
-    #     annotated_image,
-    #     results.face_landmarks,
-    #     mp_holistic.FACEMESH_TESSELATION,
-    #     landmark_drawing_spec=None,
-    #     connection_drawing_spec=mp_drawing_styles
-    #     .get_default_face_mesh_tesselation_style())
-
-    mp_drawing.draw_landmarks(
-        annotated_image,
-        results.pose_landmarks,
-        mp_holistic.POSE_CONNECTIONS,
-        landmark_drawing_spec=mp_drawing_styles.
-        get_default_pose_landmarks_style())
-    mp_drawing.draw_landmarks(
-        annotated_image,
-        results.left_hand_landmarks,
-        mp_holistic.HAND_CONNECTIONS,
-        landmark_drawing_sepc=mp_drawing_styles
-        .get_default_hand_landmarks_style())
-    mp_drawing.draw_landmarks(
-        image,
-        results.right_hand_landmarks,
-        mp_holistic.HAND_CONNECTIONS,
-        landmark_drawing_spec=mp_drawing_styles
-        .get_default_hand_landmarks_style())
-    cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
-    # Plot pose world landmarks.
-    mp_drawing.plot_landmarks(
-        results.pose_world_landmarks, mp_holistic.POSE_CONNECTIONS)
-
-# For webcam input:
-cap = cv2.VideoCapture(0)
-with mp_holistic.Holistic(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as holistic:
   while cap.isOpened():
     success, image = cap.read()
+
     if not success:
       print("Ignoring empty camera frame.")
       # If loading a video, use 'break' instead of 'continue'.
@@ -87,6 +60,8 @@ with mp_holistic.Holistic(
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = holistic.process(image)
+    #print(results)
+    RESULT_BUFFER.append(results)
 
     # Draw landmark annotation on the image.
     image.flags.writeable = True
@@ -94,35 +69,36 @@ with mp_holistic.Holistic(
 
     new_img = np.zeros(shape=(image.shape), dtype=np.uint8)
     new_img.flags.writeable = True
+    new_img[:] = BG_COLOR
 
-    # mp_drawing.draw_landmarks(
-    #     new_img,
-    #     results.face_landmarks,
-    #     mp_holistic.FACEMESH_CONTOURS,
-    #     landmark_drawing_spec=None,
-    #     connection_drawing_spec=mp_drawing_styles
-    #     .get_default_face_mesh_contours_style())
-
+    # Draw the pose-related landmarks
     mp_drawing.draw_landmarks(
         new_img,
         results.pose_landmarks,
         mp_holistic.POSE_CONNECTIONS,
         landmark_drawing_spec=mp_drawing_styles
         .get_default_pose_landmarks_style())
+
+    # Draw all hand-related landmarks
+    # Left hand
     mp_drawing.draw_landmarks(
         new_img,
         results.left_hand_landmarks,
         mp_holistic.HAND_CONNECTIONS,
         landmark_drawing_spec=mp_drawing_styles
         .get_default_hand_landmarks_style())
+    
+    # Right hand
     mp_drawing.draw_landmarks(
         new_img,
         results.right_hand_landmarks,
         mp_holistic.HAND_CONNECTIONS,
         landmark_drawing_spec=mp_drawing_styles
         .get_default_hand_landmarks_style())
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Holistic', cv2.flip(new_img, 1))
+
+    cv2.imshow('Full Body Tracking', cv2.flip(new_img, 1))
     if cv2.waitKey(5) & 0xFF == 27:
       break
+
+process_results()
 cap.release()
